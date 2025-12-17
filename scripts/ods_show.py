44598a858cc9c7cffb725902461b9cf95f7c1d0a
DAVID DEBOER <ddeboer@berkeley.edu>
2025-12-16 22:41:16 -0800

    updating ods_show

--- scripts/ods_show.py
+++ scripts/ods_show.py
@@ -5,7 +5,6 @@ from odsutils import ods_engine
 ap = argparse.ArgumentParser()
 ap.add_argument('inputs', help='Inputs to show', nargs='?', default=None)
 ap.add_argument('-s', '--sys', help='Show available system defaults files', action='store_true')
-ap.add_argument('--defaults', help='Show default values', action='store_true')
 args = ap.parse_args()
 
 if args.inputs is None:
@@ -20,6 +19,6 @@ else:
     if args.sys:
         args.inputs = f"${args.inputs}"
     ods = ods_engine.ODS(version='latest', defaults=args.inputs)
-    print(args.defaults)
+    print(args.inputs)
     for k, v in ods.defaults.items():
         print(f"\t{k}: {v}")
