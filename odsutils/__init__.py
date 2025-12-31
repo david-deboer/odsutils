135f43b7f7ff76f97ca87c1723d3bf9f433bbd21
DAVID DEBOER <ddeboer@berkeley.edu>
2025-12-31 14:45:37 -0800

    remove setup.py

--- odsutils/__init__.py
+++ odsutils/__init__.py
@@ -1,5 +1,6 @@
 from os.path import join
-__version__ = "1.0.0"
+from importlib.metadata import version
+__version__ = version('odsutils')
 
 try:
     import warnings
--- setup.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- mode: python; coding: utf-8 -*-
-# Copyright 2024 David DeBoer
-
-from setuptools import setup
-
-if __name__ == '__main__':
-    setup()
