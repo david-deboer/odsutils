a71886d47812120fa3df982f2b5d36ddb3162f69
DAVID DEBOER <ddeboer@berkeley.edu>
2026-03-02 09:26:49 -0800

    add POSTING to package init

--- odsutils/__init__.py
+++ odsutils/__init__.py
@@ -14,3 +14,15 @@ DATA_PATH = join(__path__[0], 'data')
 LOG_FORMATS = {'conlog_format': "{asctime} - {levelname} - {module} - {message}",
                'filelog_format': "{asctime} - {levelname} - {module} - {message}"}
 LOG_FILENAME = 'odslog'
+
+
+POSTING = {
+    'testing': {
+        'project': './test_proj.json',
+        'upload': './test_upload.json'
+        },
+    'ata': {
+        'project': "/opt/mnt/share/ods_project/ods_rados.json",
+        'upload': "/opt/mnt/share/ods_upload/ods.json"
+        }
+     }
